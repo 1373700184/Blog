55bb85808151a61911f23a7c80eb58e6ba97f92e
The Talent <The Talent@DESKTOP-VMKKONQ>
2020-11-30 20:00:04 +0800

    -----

--- Controllers/MixController.cs
+++ Controllers/MixController.cs
@@ -2,20 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.Other;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers
 {
-    public class Myself : Controller
+    public class MixController : Controller
     {
-
+        private readonly Logger logger;
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult Edit()
+        public IActionResult EditMix()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        public JsonResult GetMixsbyLevel()
+        {
+            var level = Request;
+            return Json(0);
         }
     }
 }
---
+++ Models/Mix.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models
+{
+    public class Mix
+    {
+        public int PKID { get; set; }
+
+        /// <summary>
+        /// 编号
+        /// </summary>
+        public string MixNo { get; set; }
+
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string MixName { get; set; }
+
+        /// <summary>
+        /// 上一级的Id
+        /// </summary>
+        public int ParentID { get; set; }
+
+        /// <summary>
+        /// 上一级的名称
+        /// </summary>
+        public string ParentName { get; set; }
+        
+        /// <summary>
+        /// 等级
+        /// </summary>
+        public int MixLevel { get; set; }
+
+        /// <summary>
+        /// 允许添加下一级
+        /// </summary>
+        public bool Allowadd { get; set; }
+
+        /// <summary>
+        /// 允许被编辑
+        /// </summary>
+        public bool Allowedit { get; set; }
+
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        public int CreatorId { get; set; }
+
+
+        /// <summary>
+        /// 状态 0 正常 -1删除
+        /// </summary>
+        public int Status { get; set; }
+
+        public DateTime UpdateTime { get; set; }
+        public DateTime AddTime { get; set; }
+        public DateTime DeleteTime { get; set; }
+
+    }
+}
