af2a1808a80c40e8f0bfec37109e4b21ff808504
1373700184 <1373700184@qq.com>
2020-12-03 17:29:31 +0800

    999

--- Controllers/BlogerController.cs
+++ Controllers/BlogerController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Web;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Blog.Models;
+using System.IO;
 
 namespace Blog.Controllers
 {
@@ -12,9 +16,15 @@ namespace Blog.Controllers
         {
             return View();
         }
-        public IActionResult EditBloger()
+        public IActionResult EditBloger(Bloger bloger,IFormFile file)
         {
-            return View();
+            string filehead = DateTime.Now.ToString("yyyyMMddHHmmss");
+          
+                string pp = "../upload/ " + filehead + file.FileName;
+                file.CopyTo(new FileStream(pp, FileMode.Create));
+                string reurl = "../upload/" + Path.GetFileName(file.FileName);
+        
+            return View(bloger);
         }
     }
 }
